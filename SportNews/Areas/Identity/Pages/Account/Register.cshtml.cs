d99e9fe68c18849f9512501b1d5dd13422228aac
vasarostik <ros666666@gmail.com>
2019-08-26 15:30:17 +0300

    Add "visit email page" after registration

--- SportNews/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ SportNews/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -83,7 +83,7 @@ namespace SportNews.Areas.Identity.Pages.Account
                         $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
                     //await _signInManager.SignInAsync(user, isPersistent: false);
-                    return LocalRedirect(returnUrl);
+                    return RedirectToPage("./VisitEmail");
                 }
                 foreach (var error in result.Errors)
                 {
---
+++ SportNews/Areas/Identity/Pages/Account/VisitEmail.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SportNews.Areas.Identity.Pages.Account
+{
+    [AllowAnonymous]
+    public class VisitEmailModel : PageModel
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public VisitEmailModel(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+
+    }
+}
